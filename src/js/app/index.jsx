db95229fc1c11166b5c6230ec3107015fc168ab5
kactusovi4 <andreyievlev2609@gmail.com>
2024-04-22 19:31:28 +0300

    final commit(but will divilop after exam)

--- src/js/app/index.jsx
+++ src/js/app/index.jsx
@@ -2,13 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
+import Footer from "./components/footer/Footer";
 
 const root = ReactDOM.createRoot(document.getElementById("react-root"));
+const footer = ReactDOM.createRoot(document.getElementById("footer"));
 
 root.render(
   <React.StrictMode>
     <Router>
       <App />
     </Router>
+  </React.StrictMode>
+);
+
+footer.render(
+  <React.StrictMode>
+    <Footer />
   </React.StrictMode>
 );
